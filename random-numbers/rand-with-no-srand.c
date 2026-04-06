270c0fd7e8daa4c86c16e4214312aa4263a604fa
Alexey Safronov <vonorfasyexela@gmail.com>
2026-04-06 14:51:58 +0300

    пример на генерацию рандомных чисел

---
+++ random-numbers/rand-with-no-srand.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    // В таком виде эта программа всегда будет выводить одно и то же число.
+    int random_number = rand();
+    printf("Случайное число БЕЗ инициализации: %d\n", random_number);
+
+    random();
+}
